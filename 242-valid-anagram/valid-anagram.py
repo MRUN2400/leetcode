b088a2050e0d1b900e16e8a10562974cbb58d80a
MRUN2400 <mrunmayeejoshi11@gmail.com>
2026-06-06 13:17:06 +0530

    Time: 10 ms (78.40%) | Memory: 19.3 MB (94.36%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq1 = {}
+        freq2 = {}
+        for ch in s:
+          if ch in freq1:
+           freq1[ch]+=1
+          else:
+            freq1[ch]=1
+        for ch in t:
+           if ch in freq2:
+             freq2[ch]+=1
+           else:
+             freq2[ch]=1
+        if freq1==freq2:
+          return True
+        return False
